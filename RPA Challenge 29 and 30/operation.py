755272cae647eee0c31c1da2e952d3f3d4e9ffef
Rathna-praba <82765951+Rathna-praba@users.noreply.github.com>
2021-05-31 20:13:59 +0530

    27.5.21 and 28.5.21

---
+++ RPA Challenge 29 and 30/operation.py
@@ -0,0 +1,16 @@
+def add_numbers(x,y):  
+    add = x+y
+    return add
+    
+num1 = 67
+num2 = 56
+print("Addition is: ",add_numbers(num1,num2))
+
+
+def sub_numbers(a,b):
+   sub = a - b
+   return sub
+
+num1 = 356
+num2 = 90
+print("Subtraction is: ", sub_numbers(num1, num2))
